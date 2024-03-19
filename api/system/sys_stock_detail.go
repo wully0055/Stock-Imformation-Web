5296e2296358c45be443a8725a69f43812c3c3ab
Willie <willy2938087@gmail.com>
2024-03-20 00:06:35 +0800

    新增上市上櫃判斷,加速進入股票詳細資料頁面時間

--- api/system/sys_stock_detail.go
+++ api/system/sys_stock_detail.go
@@ -20,22 +20,23 @@ type StockImformation struct {
 	PEratio       string `json:"PEratio"`
 	DividendYield string `json:"DividendYield"`
 	PBratio       string `json:"PBratio"`
+	Type          string `json:"type"`
 }
 
 type StockImformation2 struct {
-	//Date                  string `json:"Date"`
 	Code          string `json:"SecuritiesCompanyCode"`
 	Name          string `json:"CompanyName"`
 	PEratio       string `json:"PriceEarningRatio"`
 	DividendYield string `json:"YieldRatio"`
 	PBratio       string `json:"PriceBookRatio"`
-	//DividendPerShare      string `json:"DividendPerShare"`
+	Type          string `json:"type"`
 }
 
 type StockCode struct {
 	Code    string `json:"code"`
 	Name    string `json:"name"`
 	PEratio string `json:"peratio"`
+	Type    string `json:"type"`
 }
 
 type FavoritedStock struct {
@@ -117,6 +118,10 @@ func (s *StockDetail) StockImformation(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse external API response"})
 		return
 	}
+	// 在每個 StockImformation 中添加 type 字段並設置為 "上市"
+	for i := range stocks {
+		stocks[i].Type = "上市"
+	}
 	//上櫃資料
 	apiURL2 := "https://www.tpex.org.tw/openapi/v1/tpex_mainboard_peratio_analysis"
 	resp2, err := http.Get(apiURL2)
@@ -131,6 +136,10 @@ func (s *StockDetail) StockImformation(c *gin.Context) {
 	if err := json.NewDecoder(resp2.Body).Decode(&stocks2); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse external API response"})
 		return
+	}
+	// 在每個 StockImformation2 中添加 type 字段並設置為 "上櫃"
+	for i := range stocks2 {
+		stocks2[i].Type = "上櫃"
 	}
 
 	merge_data := mergeStructs(stocks, stocks2)
@@ -152,6 +161,7 @@ func mergeStructs(data1 []StockImformation, data2 []StockImformation2) []StockIm
 			PEratio:       entry.PEratio,
 			DividendYield: entry.DividendYield,
 			PBratio:       entry.PBratio,
+			Type:          entry.Type,
 		}
 		mergedData = append(mergedData, newdata1)
 
@@ -167,8 +177,15 @@ func (s *StockDetail) StockData(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// 處理 requestData
-	apiURL := "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_" + requestData.Code + ".tw"
+	var apiURL string
+
+	if requestData.Type == "上市" {
+		// 處理 requestData
+		apiURL = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_" + requestData.Code + ".tw"
+	} else {
+		// 處理 requestData
+		apiURL = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=otc_" + requestData.Code + ".tw"
+	}
 
 	resp, err := http.Get(apiURL)
 	if err != nil {
@@ -222,6 +239,7 @@ func (s *StockDetail) StockData(c *gin.Context) {
 		ValuesMap["本益比"] = requestData.PEratio
 		ValuesMap["EPS"] = eps
 		ValuesMap["合理價"] = eps_str
+		ValuesMap["類型"] = "上市"
 	}
 
 	c.JSON(http.StatusOK, ValuesMap)
@@ -230,74 +248,74 @@ func (s *StockDetail) StockData(c *gin.Context) {
 }
 
 // StockData2 上櫃股票詳細資料
-func (s *StockDetail) StockData2(c *gin.Context) {
-
-	var requestData StockCode
-	if err := c.BindJSON(&requestData); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	// 處理 requestData
-	apiURL2 := "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=otc_" + requestData.Code + ".tw"
-
-	resp, err := http.Get(apiURL2)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch external API"})
-		return
-	}
-	defer resp.Body.Close()
-
-	// 解析API的JSON響應
-	var apiResponse APIResponse
-
-	// 僅解碼 JSON 數據中的 msgArray 字段
-	if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse external API response"})
-		return
-	}
-
-	eps := StockEPS(requestData.Code)
-
-	floateps, err := strconv.ParseFloat(eps, 64)
-	if err != nil {
-		fmt.Println("eps轉換float失敗:", err)
-		return
-	}
-	//若沒有本益比資料，本益比設為 0
-	var stockPEratio string
-	stockPEratio = requestData.PEratio
-	if stockPEratio == "" || stockPEratio == "N/A" {
-		stockPEratio = "0"
-	}
-
-	floatPEratio, err := strconv.ParseFloat(stockPEratio, 64)
-	if err != nil {
-		fmt.Println("本益比轉換float失敗:", err)
-		return
-	}
-	price := floateps * floatPEratio
-	eps_str := fmt.Sprintf("%.3f", price)
-
-	ValuesMap := make(map[string]string)
-	for _, item := range apiResponse.MsgArray {
-		ValuesMap["股票代號"] = item.C
-		ValuesMap["公司簡稱"] = item.N
-		ValuesMap["成交價"] = item.Z
-		ValuesMap["成交量"] = item.Tv
-		ValuesMap["累積成交量"] = item.V
-		ValuesMap["開盤價"] = item.O
-		ValuesMap["最高價"] = item.H
-		ValuesMap["最低價"] = item.L
-		ValuesMap["昨收價"] = item.Y
-		ValuesMap["本益比"] = requestData.PEratio
-		ValuesMap["EPS"] = eps
-		ValuesMap["合理價"] = eps_str
-	}
-
-	c.JSON(http.StatusOK, ValuesMap)
-
-	// 返回響應
-}
+//func (s *StockDetail) StockData2(c *gin.Context) {
+//
+//	var requestData StockCode
+//	if err := c.BindJSON(&requestData); err != nil {
+//		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//		return
+//	}
+//	// 處理 requestData
+//	apiURL2 := "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=otc_" + requestData.Code + ".tw"
+//
+//	resp, err := http.Get(apiURL2)
+//	if err != nil {
+//		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch external API"})
+//		return
+//	}
+//	defer resp.Body.Close()
+//
+//	// 解析API的JSON響應
+//	var apiResponse APIResponse
+//
+//	// 僅解碼 JSON 數據中的 msgArray 字段
+//	if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
+//		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse external API response"})
+//		return
+//	}
+//
+//	eps := StockEPS(requestData.Code)
+//
+//	floateps, err := strconv.ParseFloat(eps, 64)
+//	if err != nil {
+//		fmt.Println("eps轉換float失敗:", err)
+//		return
+//	}
+//	//若沒有本益比資料，本益比設為 0
+//	var stockPEratio string
+//	stockPEratio = requestData.PEratio
+//	if stockPEratio == "" || stockPEratio == "N/A" {
+//		stockPEratio = "0"
+//	}
+//
+//	floatPEratio, err := strconv.ParseFloat(stockPEratio, 64)
+//	if err != nil {
+//		fmt.Println("本益比轉換float失敗:", err)
+//		return
+//	}
+//	price := floateps * floatPEratio
+//	eps_str := fmt.Sprintf("%.3f", price)
+//
+//	ValuesMap := make(map[string]string)
+//	for _, item := range apiResponse.MsgArray {
+//		ValuesMap["股票代號"] = item.C
+//		ValuesMap["公司簡稱"] = item.N
+//		ValuesMap["成交價"] = item.Z
+//		ValuesMap["成交量"] = item.Tv
+//		ValuesMap["累積成交量"] = item.V
+//		ValuesMap["開盤價"] = item.O
+//		ValuesMap["最高價"] = item.H
+//		ValuesMap["最低價"] = item.L
+//		ValuesMap["昨收價"] = item.Y
+//		ValuesMap["EPS"] = eps
+//		ValuesMap["合理價"] = eps_str
+//		ValuesMap["類型"] = "上櫃"
+//	}
+//
+//	c.JSON(http.StatusOK, ValuesMap)
+//
+//	// 返回響應
+//}
 
 // StockEPS 取得近四次EPS總和
 func StockEPS(value string) string {
@@ -358,6 +376,7 @@ func StockTableData(merge_data []StockImformation) {
 			data := system.SysStockTable{
 				StockID:   response.Code,
 				StockName: response.Name,
+				Type:      response.Type,
 			}
 			result := db.Create(&data)
 			if result.Error != nil {
@@ -418,5 +437,24 @@ func (s *StockDetail) MyFavorited(c *gin.Context) {
 	db := global.SKW_DB
 	var data []system.SysMyFavourite
 	db.Find(&data)
-	c.JSON(http.StatusOK, data)
+	var result []map[string]interface{}
+
+	for _, favorite := range data {
+		var stock system.SysStockTable
+		if err := db.Where("stock_id = ?", favorite.StockID).First(&stock).Error; err != nil {
+			// 處理錯誤
+			continue
+		}
+
+		// 將兩個結構體的欄位合併成一個 map
+		item := map[string]interface{}{
+			"code":    stock.StockID,
+			"name":    stock.StockName,
+			"peratio": favorite.PEratio,
+			"type":    stock.Type,
+		}
+		result = append(result, item)
+	}
+
+	c.JSON(http.StatusOK, result)
 }
--- model/system/sys_stocktable.go
+++ model/system/sys_stocktable.go
@@ -8,7 +8,7 @@ type SysStockTable struct {
 	global.SKW_MODEL
 	StockID   string `json:"code" gorm:"index;comment:股票代號"`
 	StockName string `json:"name" gorm:"index;comment:股票名稱"`
-	//PEratio   string `json:"peratio" gorm:"index;comment:本益比"`
+	Type      string `json:"type" gorm:"index;comment:上市/上櫃"`
 }
 
 func (SysStockTable) TableName() string {
