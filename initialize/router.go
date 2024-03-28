8ef196bea688d1c19d342b1cc996f5ec987848ca
Willie <willy2938087@gmail.com>
2024-03-29 04:02:35 +0800

    更改啟動後的入口位置

--- initialize/router.go
+++ initialize/router.go
@@ -4,6 +4,8 @@ import (
 	"Stock-Imformation-Web/router"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"os/exec"
+	"runtime"
 )
 
 func Routers() *gin.Engine {
@@ -13,12 +15,41 @@ func Routers() *gin.Engine {
 
 	systemRouter := router.RouterGroupApp.System
 
+	// 設置靜態文件目錄
+	Router.StaticFile("/", "index.html")
+
+	Router.LoadHTMLGlob("templates/*")
+
+	Router.GET("templates/stockdata.html", func(c *gin.Context) {
+		c.File("templates/stockdata.html")
+	})
+	Router.GET("templates/myfavorited.html", func(c *gin.Context) {
+		c.File("templates/myfavorited.html")
+	})
+
 	PublicGroup := Router.Group("test")
 
 	systemRouter.InitApiRouter(PublicGroup)
+
+	// 在此之前打開瀏覽器
+	openBrowser("http://localhost:8080")
 
 	Router.Run(":8080")
 
 	return Router
 
+}
+
+// openBrowser 根據給定的 URL 使用系統預設的瀏覽器打開網頁
+func openBrowser(url string) error {
+	var cmd string
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = "open"
+	case "windows":
+		cmd = "start"
+	default:
+		cmd = "xdg-open"
+	}
+	return exec.Command(cmd, url).Start()
 }
