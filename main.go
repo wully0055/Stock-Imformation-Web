f3f8311c71c660a604d700f81dbe053ec4eebce9
Willie <willy2938087@gmail.com>
2024-04-08 02:03:51 +0800

    修改一些code排版

--- main.go
+++ main.go
@@ -7,31 +7,6 @@ import (
 
 func main() {
 
-	// 創建一個Gin引擎
-	//r := gin.Default()
-
-	// Use CORS middleware
-	//r.Use(cors.Default())
-
-	//r.GET("/getExternalAPI", func(c *gin.Context) {
-	//
-	//	// 调用外部API获取数据
-	//	apiData, err := fetchDataFromExternalAPI()
-	//	if err != nil {
-	//		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	//		return
-	//	}
-	//	epsData := filterDataByType(apiData, "EPS")
-	//
-	//	c.JSON(http.StatusOK, epsData)
-	//
-	//	// 將API的結果傳遞到前端
-	//})
-
-	// 啟動Gin服務器
-	//r.Run(":8080")
-
-	//
 	global.SKW_DB = initialize.Gorm()
 
 	if global.SKW_DB != nil {
